0c2b9f05d5024be4c1fffbcaa27d05468030fa40
parks <parks@DESKTOP-Q6NOLHU>
2022-01-30 00:50:12 +0900

    1.프로젝트 생성

---
+++ ArtistCrud/src/main/java/com/sm/artist/ArtistCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sm.artist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistCrudApplication.class, args);
+	}
+
+}
---
+++ ArtistCrud/src/test/java/com/sm/artist/ArtistCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.artist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
