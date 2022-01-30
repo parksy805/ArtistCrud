ef6179a01725ece3beccd06f9ffcf58ff6cd3b77
parks <parks@DESKTOP-Q6NOLHU>
2022-01-30 16:44:55 +0900

    5.GET getAllArtists()_void   GET getAllArtists()_jsp전송

--- ArtistCrud/src/main/java/com/sm/artist/component/ArtistController.java
+++ ArtistCrud/src/main/java/com/sm/artist/component/ArtistController.java
@@ -1,9 +1,11 @@
 package com.sm.artist.component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,11 +17,53 @@ public class ArtistController {
 	@Autowired
 	private ArtistService artistService;
 
-	@GetMapping("/artists") // GET으로 모든 아티스트들의 정보를 json 반환 
-	@ResponseBody  // 자바 오브젝트를 자동으로 JSON으로 변환해서 리턴
+	@GetMapping("/artists") // GET으로 모든 아티스트들의 정보를 json 반환
+	@ResponseBody // 자바 오브젝트를 자동으로 JSON으로 변환해서 리턴
 	public List<ArtistVo> getAllArtistsJson() {
 		return artistService.getAllArtists();
 	}
+
+	@GetMapping("/artists.void") // 아티스트 정보를 console에 출력
+	public void getAllArtists() {
+		List<ArtistVo> artists = artistService.getAllArtists();
+
+		System.out.println("List 출력 === \n" + artists.toString());
+		System.out.println();
+
+		ArrayList<String> artistName = new ArrayList<String>();
+		// artistName변수는 foreach반복문 블록 바깥에서 먼저 선언해줘야 블록안에서 add된 값을 가져올수 있음
+		for (ArtistVo artist : artists) {
+			System.out.println("한줄 씩 ArtistVo 객체 출력 === " + artist.toString());
+			artistName.add(artist.getArtistName());
+		}
+		System.out.println();
+
+		System.out.println("artistName.toString() === " + artistName.toString());
+		System.out.println("artistName.get(0) === " + artistName.get(0));
+
+	}
 	
-	
+	@GetMapping("/artists.do") // GET으로 아티스트들의 정보를 Jsp로 전송
+	public String getAllArtistsJsp(Model model) {
+		List<ArtistVo> artists = artistService.getAllArtists();
+		System.out.println(artists.get(0).getArtistName());
+		// List타입.get(인덱스).getter함수()
+		
+		ArrayList<String> artistsName = new ArrayList<String>();
+		// ArrayList타입의 변수는 foreach반복문 블록 바깥에서 먼저 선언해줘야
+		// foreach반복문 블록안에서 add된 값을 가져올수 있음
+		for (ArtistVo artist : artists) {
+			artistsName.add(artist.getArtistName());
+		}
+		
+		ArtistVo siwumin = new ArtistVo();
+		siwumin.setArtistGroup(artists.get(3).getArtistGroup());
+		
+		model.addAttribute("artists", artists);
+		model.addAttribute("artistsName", artistsName);
+		model.addAttribute("siwumin", siwumin);
+		
+		return "index";
+	}
+
 }
--- ArtistCrud/src/main/webapp/WEB-INF/view/index.jsp
+++ ArtistCrud/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,37 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>Web 연결 성공</h1> 
+	<h1>Web 연결 성공</h1>
+	<h3>List타입 artists 출력</h3>
+	${artists}
+	<hr>
+	
+	<h3>List타입 c:forEach > option 태그로 출력</h3>
+	<c:forEach var="a" items="${artists}">
+		<option value="${a.artistName}">${a.artistName}</option>
+	</c:forEach>
+	<hr>
+	
+	<h3>List타입 c:forEach > div 태그로 출력</h3>
+	<c:forEach var="a" items="${artists}">
+		<div>${a.artistName}</div>
+	</c:forEach>
+	
+	<h3>List타입 c:forEach 태그로 출력</h3>
+	<c:forEach var="a" items="${artists}"> 
+		${a.artistName} 
+	</c:forEach>
+	<hr>
+	
+	<h3>ArrayList타입 {artistsName} 출력</h3>
+	<div>${artistsName}</div>
+	<br>
+	
+	<h3>ArtistVo타입 {siwumin} 출력</h3>
+	<div>${siwumin}</div>
+	ㄴ DB의 KimMinsuk이 있는 객체의 artistNo는 4지만
+	<br> 기존 데이터 객체에서 getter한게 아니라
+	<br> new ArtistVo() 새로 생성된 객체에 name,group만 setter한걸 가져와서
+	<br> 초기값으로 초기화된 0이 뜨는 것
 </body>
 </html>
